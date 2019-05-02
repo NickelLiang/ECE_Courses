80f1410cd8df7311d5036d3ce536d079b8865d88
Nickel Liang <zuodong2@illinois.edu>
2019-05-02 13:01:32 -0500

    gemm working

--- ECE408/ece408_src/new-forward.cuh
+++ ECE408/ece408_src/new-forward.cuh
@@ -3,8 +3,9 @@
 
 #include <mxnet/base.h>
 
-#define TILE_WIDTH              16
+#define TILE_WIDTH              25
 #define SHARE_MEM_SIZE          ((TILE_WIDTH + _K - 1) * (TILE_WIDTH + _K - 1))
+#define MAX_THREADS             1024
 
 #define _K                      5
 
@@ -204,7 +205,6 @@ __global__ void forward_kernel_layer_1(float *y, const float *x, const int B) {
 
     // Do conv
     float acc = 0.0;
-    // if ((h < _H1_OUT) && (w < _W1_OUT) && (b < B) && (m < _M1)) {
     if ((h < _H1_OUT) && (w < _W1_OUT)) {
         for (int p = 0; p < _K; p++)
             for (int q = 0; q < _K; q++)
@@ -243,7 +243,6 @@ __global__ void forward_kernel_layer_2(float *y, const float *x, const int B) {
 
     // Do conv
     float acc = 0.0;
-    // if ((h < _H2_OUT) && (w < _W2_OUT) && (b < B) && (m < _M2)) {
     if ((h < _H2_OUT) && (w < _W2_OUT)) {
         for (int c = 0; c < _C2; c++)
             for (int p = 0; p < _K; p++)
@@ -254,6 +253,92 @@ __global__ void forward_kernel_layer_2(float *y, const float *x, const int B) {
     #undef y4d
     #undef x4d
     #undef k4d
+}
+
+__global__ void gemm_unroll(int b, const int C, const int H, const int W, const int K, float *x, float *x_unroll) {
+    int thread = blockIdx.x * blockDim.x + threadIdx.x;
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
+    int W_unroll_size = H_out * W_out;
+
+    if (thread < C * K * K * W_unroll_size) {
+        int row = thread / W_unroll_size;
+        int col = thread % W_unroll_size;
+        int kernel_row = row / K;
+        x_unroll[thread] = x[b * C * H * W + (kernel_row / K) * H * W + (col / W_out + (kernel_row % K)) * W + (col % W_out + row % K)];
+    }
+}
+
+// This code is from MP3 with minor modification
+// matrixMultiplyShared<<<gridDim, blockDim>>>(w.dptr_, x_unroll, y.dptr_, K, H_unroll, H_unroll, W_unroll, M, W_unroll, b);
+__global__ void matrixMultiplyShared(float *A, float *B, float *C, int numARows, int numAColumns, int numBRows, int numBColumns, int numCRows, int numCColumns, int b) {
+    if (numAColumns != numBRows) return;
+
+    __shared__ float subTileA[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float subTileB[TILE_WIDTH][TILE_WIDTH];
+
+    float value = 0;
+    int row = blockIdx.y * TILE_WIDTH + threadIdx.y;
+    int col = blockIdx.x * TILE_WIDTH + threadIdx.x;
+
+    int offset = b * numCRows * numCColumns;
+
+    for (int m = 0; m < ceil(numAColumns / (float)(TILE_WIDTH)); m++){
+        int row_m = m * TILE_WIDTH + threadIdx.y;
+        int col_m = m * TILE_WIDTH + threadIdx.x;
+
+        if (col_m < numAColumns)
+            subTileA[threadIdx.y][threadIdx.x] = A[row * numAColumns + col_m];
+        else
+            subTileA[threadIdx.y][threadIdx.x] = 0.0;
+
+        if (row_m < numBRows)
+            subTileB[threadIdx.y][threadIdx.x] = B[row_m * numBColumns + col];
+        else
+            subTileB[threadIdx.y][threadIdx.x] = 0.0;
+
+        __syncthreads();
+        for (int k = 0; k < TILE_WIDTH; k++)
+            value += subTileA[threadIdx.y][k] * subTileB[k][threadIdx.x];
+        __syncthreads();
+    }
+
+    if ((row < numCRows) && (col < numCColumns))
+        C[offset + row * numCColumns + col] = value;
+}
+
+__global__ void forward_kernel_fusion(const int C, const int H, const int W, const int M, float *k, float *x, float *y, int row_a, int col_a, int row_b, int col_b, int row_c, int col_c) {
+    __shared__ float tile_m[TILE_WIDTH][TILE_WIDTH]; // M for x shared
+    __shared__ float tile_n[TILE_WIDTH][TILE_WIDTH]; // N for k shared
+
+    int H_out = H - _K + 1;
+    int W_out = W - _K + 1;
+    int K_unroll = _K * _K;
+    int row = blockIdx.y * TILE_WIDTH + threadIdx.y;
+    int col = blockIdx.z * TILE_WIDTH + threadIdx.x;
+
+    float acc = 0.0;
+
+    for (int i = 0; i < ceil(col_a / (float)(TILE_WIDTH)); ++i) {
+        int base = i * TILE_WIDTH;
+        int chan = (base + threadIdx.y) / K_unroll;
+        if ((col < col_b) && (base + threadIdx.y < row_b))
+            tile_m[threadIdx.y][threadIdx.x] = x[blockIdx.x * (C * H * W) + chan * H * W + (col / W_out) * W + (col % W_out)];
+        else
+            tile_m[threadIdx.y][threadIdx.x] = 0;
+
+        if ((row < row_a) && (base + threadIdx.x < col_a))
+            tile_n[threadIdx.y][threadIdx.x] = k[row * C * K_unroll + chan * K_unroll + ((base + threadIdx.y) % K_unroll)];
+        else
+            tile_n[threadIdx.y][threadIdx.x] = 0;
+        __syncthreads();
+
+        for (int j = 0; j < TILE_WIDTH; ++j)
+            acc += tile_m[j][threadIdx.x] * tile_n[threadIdx.y][j];
+        __syncthreads();
+    }
+    if (row < row_c && col < col_c)
+        y[blockIdx.x * (M * H_out * W_out) + row * col_c + col] = acc;
 }
 
 /*
@@ -296,19 +381,51 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     // forward_kernel_share_mem_const_kernel<<<grid_dim, block_dim, share_mem_size>>>(y.dptr_, x.dptr_, w.dptr_, B, M, C, H, W, K);
 
     // 3: Multiple kernel implementations for different layer sizes; TILE_1 = 22; TILE_2 = 9; 14.899ms;
-    if (C == _C1) {
-        cudaMemcpyToSymbol(KERNEL_1, w.dptr_, CONST_KERNEL_SIZE_1 * sizeof(float));
-        dim3 grid_dim(B, M, ceil(H_out / (float)(TILE_WIDTH_1)) * ceil(W_out / (float)(TILE_WIDTH_1)));
-        dim3 block_dim(TILE_WIDTH_1, TILE_WIDTH_1, 1);
-        int share_mem_size = SHARE_MEM_SIZE_1 * sizeof(float);
-        forward_kernel_layer_1<<<grid_dim, block_dim, share_mem_size>>>(y.dptr_, x.dptr_, B);
-    } else if (C == _C2) {
-        cudaMemcpyToSymbol(KERNEL_2, w.dptr_, CONST_KERNEL_SIZE_2 * sizeof(float));
-        dim3 grid_dim(B, M, ceil(H_out / (float)(TILE_WIDTH_2)) * ceil(W_out / (float)(TILE_WIDTH_2)));
-        dim3 block_dim(TILE_WIDTH_2, TILE_WIDTH_2, 1);
-        int share_mem_size = SHARE_MEM_SIZE_2 * sizeof(float);
-        forward_kernel_layer_2<<<grid_dim, block_dim, share_mem_size>>>(y.dptr_, x.dptr_, B);
+    // if (C == _C1) {
+    //     cudaMemcpyToSymbol(KERNEL_1, w.dptr_, CONST_KERNEL_SIZE_1 * sizeof(float));
+    //     dim3 grid_dim(B, M, ceil(H_out / (float)(TILE_WIDTH_1)) * ceil(W_out / (float)(TILE_WIDTH_1)));
+    //     dim3 block_dim(TILE_WIDTH_1, TILE_WIDTH_1, 1);
+    //     int share_mem_size = SHARE_MEM_SIZE_1 * sizeof(float);
+    //     forward_kernel_layer_1<<<grid_dim, block_dim, share_mem_size>>>(y.dptr_, x.dptr_, B);
+    // } else if (C == _C2) {
+    //     cudaMemcpyToSymbol(KERNEL_2, w.dptr_, CONST_KERNEL_SIZE_2 * sizeof(float));
+    //     dim3 grid_dim(B, M, ceil(H_out / (float)(TILE_WIDTH_2)) * ceil(W_out / (float)(TILE_WIDTH_2)));
+    //     dim3 block_dim(TILE_WIDTH_2, TILE_WIDTH_2, 1);
+    //     int share_mem_size = SHARE_MEM_SIZE_2 * sizeof(float);
+    //     forward_kernel_layer_2<<<grid_dim, block_dim, share_mem_size>>>(y.dptr_, x.dptr_, B);
+    // }
+
+
+
+
+
+
+    int H_unroll = C * K * K;
+    int W_unroll = H_out * W_out;
+
+    float* x_unroll;
+    cudaMalloc((void **)&x_unroll, W_unroll * H_unroll * sizeof(float));
+
+    dim3 blockDim(TILE_WIDTH, TILE_WIDTH);
+    dim3 gridDim(ceil(W_unroll / (float)(TILE_WIDTH)), ceil(M / (float)(TILE_WIDTH)));
+    for (int b = 0; b < B; b++) {
+        gemm_unroll<<<ceil((W_unroll * H_unroll) / (float)MAX_THREADS), MAX_THREADS>>>(b, C, H, W, K, x.dptr_, x_unroll);
+        matrixMultiplyShared<<<gridDim, blockDim>>>(w.dptr_, x_unroll, y.dptr_, K, H_unroll, H_unroll, W_unroll, M, W_unroll, b);
     }
+    cudaFree(x_unroll);
+
+
+
+
+    // int H_unroll = C * K * K;
+    // int W_unroll = H_out * W_out;
+    // dim3 block_dim(TILE_WIDTH, TILE_WIDTH, 1);
+    // dim3 grid_dim(B, ceil(W_unroll / (float)(TILE_WIDTH)), ceil(M / (float)(TILE_WIDTH)));
+    // forward_kernel_fusion<<<grid_dim, block_dim>>>(C, H, W, M, w.dptr_, x.dptr_, y.dptr_, M, W_unroll, W_unroll, H_unroll, M, H_unroll);
+
+
+
+
 
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
