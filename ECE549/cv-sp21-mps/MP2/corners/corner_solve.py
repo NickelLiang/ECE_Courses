9f416d94e671ac4f836ef7103cfb5fc04ecca2b8
Nickel Liang <zuodong2@illinois.edu>
2021-03-19 17:25:22 -0500

    finish corner

--- ECE549/cv-sp21-mps/MP2/corners/corner_solve.py
+++ ECE549/cv-sp21-mps/MP2/corners/corner_solve.py
@@ -1,5 +1,63 @@
 import numpy as np
 import scipy
+import cv2
+
+# import matplotlib
+# matplotlib.use("TkAgg")
+# from matplotlib import pyplot as plt
+
+def rgb_2_gray(I):
+  I = cv2.cvtColor(I, cv2.COLOR_BGR2GRAY)
+  I = I.astype(np.float32)/255.
+  return I
+
+def gradient(I, gtype='conv'):
+  if (gtype == 'sobel'):
+    dx = scipy.ndimage.sobel(I, 0)
+    dy = scipy.ndimage.sobel(I, 1)
+  elif (gtype == 'conv'):
+    dx = scipy.signal.convolve2d(I, np.array([[-1, 0, 1]]), mode='same', boundary='symm')
+    dy = scipy.signal.convolve2d(I, np.array([[-1, 0, 1]]).T, mode='same', boundary='symm')
+  elif (gtype == 'numpy'):
+    dy, dx = np.gradient(I)
+  return dx, dy
+
+def gaussian_kernel(I, sigma):
+  return scipy.ndimage.gaussian_filter(I, sigma)
+
+def normalize(I):
+  return (I / np.amax(I)) * 255.0
+
+def get_response(I, k, sigma, gtype='conv'):
+  dx, dy = gradient(I, gtype)
+  i_xx = gaussian_kernel(dx**2, sigma)
+  i_yy = gaussian_kernel(dy**2, sigma)
+  i_xy = gaussian_kernel(dx*dy, sigma)
+
+  det = i_xx * i_yy - i_xy**2
+  trace = i_xx + i_yy
+  r = det - k * (trace**2)
+
+  assert(r.shape == I.shape)
+  return r
+
+def threshold_nms(response, window=15):
+  # window need to be larger than 3 and odd
+  assert(window%2 == 1)
+  assert(window >= 3)
+
+  offset = int((window - 1) / 2)
+
+  corners = np.zeros(response.shape)
+  for i in range(offset, response.shape[0]-offset):
+    for j in range(offset, response.shape[1]-offset):
+      region = response[i-offset:i+offset+1, j-offset:j+offset+1]
+      if (np.amax(region) == response[i,j]):
+        corners[i,j] = response[i,j]
+      else:
+        corners[i,j] = 0
+
+  return corners
 
 def compute_corners(I):
   # Currently this code proudces a dummy corners and a dummy corner response
@@ -14,19 +72,18 @@ def compute_corners(I):
   #   pixel stores the score for being a corner. Non-max suppressed pixels
   #   should have a low / zero-score.
   
-  rng = np.random.RandomState(int(np.sum(I.astype(np.float32))))
-  sz = I.shape[:2]
-  corners = rng.rand(*sz)
-  corners = np.clip((corners - 0.95)/0.05, 0, 1)
-  
-  response = scipy.ndimage.gaussian_filter(corners, 4, order=0, output=None,
-                                           mode='reflect')
-  corners = corners * 255.
-  corners = np.clip(corners, 0, 255)
-  corners = corners.astype(np.uint8)
-  
-  response = response * 255.
+  k, sigma, w, gtype = 0.05, 0.85, 3, 'conv'
+
+  I = rgb_2_gray(I)
+
+  response = get_response(I, k, sigma, gtype)
+  response = normalize(response)
   response = np.clip(response, 0, 255)
+
+  corners = threshold_nms(response, w)
+  corners = np.clip(corners, 0, 255)
+
   response = response.astype(np.uint8)
+  corners = corners.astype(np.uint8)
   
   return response, corners
